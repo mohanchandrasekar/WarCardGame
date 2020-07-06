69459b58921daef6f0f6276d1f97f846263d8969
Mohan Chanddrasekar <psgtech.mohan@gmail.com>
2020-07-06 17:28:52 +0200

    Initial Commit

---
+++ WarCardGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WarCardGame
+//
+//  Created by Mohan Chandrasekar  on 2020-07-06.
+//  Copyright © 2020 Mohan Chandrasekar . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
