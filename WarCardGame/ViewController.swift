4e64e24ceaf8e423496451c270241f34cd733242
Mohan Chanddrasekar <psgtech.mohan@gmail.com>
2020-07-06 20:32:07 +0200

    Card Game in IOS
    Init commit with Basic functionallity and UI for Card game

--- WarCardGame/ViewController.swift
+++ WarCardGame/ViewController.swift
@@ -10,11 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var leftCardView: UIImageView!
+    
+    @IBOutlet weak var rightCardView: UIImageView!
+    
+    @IBOutlet weak var leftPlayerScore: UILabel!
+    
+    @IBOutlet weak var rightCpuScore: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    
+    @IBAction func dealButton(_ sender: Any) {
+        
+        let leftNumber = Int.random(in: 2...14)
+        
+        let rightNumber = Int.random(in: 2...14)
+        
+        leftCardView.image = UIImage(named: "card\(leftNumber)")
+        
+        rightCardView.image = UIImage(named: "card\(rightNumber)")
+        
+    }
+    
 
 }
 
